badd0db18929fd32cecaa982e1efc11dc5c70dfd
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-01 01:33:53 +0200

    Update logger.py

--- Zaid/MusicUtilities/helpers/logger.py
+++ Zaid/MusicUtilities/helpers/logger.py
@@ -11,13 +11,13 @@ async def LOG_CHAT(message, what):
     user_name = message.from_user.first_name
     mention = "["+user_name+"](tg://user?id="+str(user_id)+")" 
     logger_text = f"""
-__**New {what}**__
+__**Baru Hasil Pencarian**__
 
-**Chat:** {message.chat.title} [`{message.chat.id}`]
-**User:** {mention}
+**Obrolan:** {message.chat.title} [`{message.chat.id}`]
+**Pengguna:** {mention}
 **Username:** @{message.from_user.username}
-**User ID:** `{message.from_user.id}`
-**Chat Link:** {chatusername}
-**Query:** {message.text}"""
+**ID Pengguna:** `{message.from_user.id}`
+**Link Obrolan:** {chatusername}
+**Pencarian:** {message.text}"""
     await ASS_ACC.send_message(LOG_GROUP_ID, f"{logger_text}", disable_web_page_preview=True)
     
